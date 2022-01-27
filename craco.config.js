fa77dfaf5c4eb850c7c06089a3914b9b76effad5
FlightLoong <1577886278@qq.com>
2022-01-28 00:15:54 +0800

    移动端适配

--- craco.config.js
+++ craco.config.js
@@ -1,4 +1,10 @@
 const path = require('path')
+const pxToViewport = require('postcss-px-to-viewport')
+
+const vw = pxToViewport({
+  // 视口宽度，一般就是 375（ 设计稿一般采用二倍稿，宽度为 375 ）
+  viewportWidth: 375
+})
 
 module.exports = {
   // webpack 配置
@@ -10,6 +16,12 @@ module.exports = {
       // 约定：使用 @scss 表示全局 SASS 样式所在路径
       // 在 SASS 中使用
       '@scss': path.resolve(__dirname, 'src/assets/styles')
+    }
+  },
+
+  style: {
+    postcss: {
+      plugins: [vw]
     }
   }
 }
