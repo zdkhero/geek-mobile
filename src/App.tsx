3b73ac5422a096742de7d06d081b6b47e9072e86
FlightLoong <1577886278@qq.com>
2022-01-27 19:08:57 +0800

    配置基础路由

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
+// 导入 React 路由
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
+// 导入组件
+import Layout from './pages/Layout'
+import Login from './pages/Login'
+
+// 导入样式文件
 import './App.scss'
 
 function App() {
   return (
-    <div className="App">
-      根组件
-    </div>
+    // 配置路由规则
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}></Route>
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
