77a82f392b93c3b5e0149c3d807b7b3319c9d619
FlightLoong <1577886278@qq.com>
2022-01-28 15:08:17 +0800

    配置 Redux 的相关类型

---
+++ src/types/store.d.ts
@@ -0,0 +1,19 @@
+import { ThunkAction } from 'redux-thunk'
+import store from '@/store'
+
+// Redux 应用的状态
+export type RootState = ReturnType<typeof store.getState>
+
+// 使用中间件后的 Redux dispatch 类型
+// ReturnType: thunk action 的返回类型
+// State：Redux 应用的状态
+// ExtraThunkArg: 额外的参数，没有用到，可以指定为 unknown
+// BasicAction: 非 thunk action，即对象形式的 action
+export type RootThunAction = ThunkAction<void, RootState, unknown, RootAction>
+
+// 项目中所有 action 类型
+type RootAction = unknown
+
+// 登录相关的 action 类型
+
+// 文章相关的 action 类型
