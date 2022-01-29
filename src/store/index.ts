89d7957a946766d91d9c6f9f256186e7102a7bff
FlightLoong <1577886278@qq.com>
2022-01-29 13:09:27 +0800

    redux 设置默认 token

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,11 +4,18 @@ import { composeWithDevTools } from 'redux-devtools-extension'
 
 // 导入根 reducer
 import rootReducer from './reducers'
+import { getToken } from '@/utils/token'
 
 // 配置 redux 调试工具
 const middlewares = composeWithDevTools(applyMiddleware(thunk))
 
+// 初始化状态
+const initalState = {
+  // 注意：此处的 login 属性是根据合并 reducer 时，login 的名称而来的
+  login: getToken()
+}
+
 // 创建 store 仓库
-const store = createStore(rootReducer, middlewares)
+const store = createStore(rootReducer, initalState, middlewares)
 
 export default store
--- src/utils/token.ts
+++ src/utils/token.ts
@@ -4,7 +4,11 @@ import { Token } from '@/types/data'
 const GEEK_TOKEN_KEY = 'geek-h5-token'
 
 // 创建 获取 token
-export const getToken = (): Token => JSON.parse(localStorage.getItem(GEEK_TOKEN_KEY) || '{}')
+// export const getToken = (): Token => JSON.parse(localStorage.getItem(GEEK_TOKEN_KEY) || '{}')
+
+// ?? 空值合并运算符的短路用法：当空值合并运算符的左表达式不为 null 或 undefined 时，不会对右表达式进行求值
+export const getToken = () =>
+  JSON.parse(localStorage.getItem(GEEK_TOKEN_KEY) ?? '{ "token": "", "refresh_token": "" }') as Token
 
 // 创建 设置 token
 export const setToken = (token: Token) => localStorage.setItem(GEEK_TOKEN_KEY, JSON.stringify(token))
