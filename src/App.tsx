1e7cba512156958a300fa69f6ad772bdd5e44c85
FlightLoong <1577886278@qq.com>
2022-01-30 03:01:42 +0800

    TabBar 配合路由使用

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 // 导入组件
 import Layout from '@/pages/Layout'
 import Login from '@/pages/Login'
+import Home from '@/pages/Home'
+import Question from '@/pages/Question'
+import Video from '@/pages/Video'
+import Profile from '@/pages/Profile'
 
 // 导入样式文件
 import './App.scss'
@@ -13,7 +17,12 @@ function App() {
     // 配置路由规则
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}></Route>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}></Route>
+          <Route path="question" element={<Question />}></Route>
+          <Route path="video" element={<Video />}></Route>
+          <Route path="profile" element={<Profile />}></Route>
+        </Route>
         <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Layout/index.tsx
+++ src/pages/Layout/index.tsx
@@ -1,5 +1,38 @@
+import Icon from '@/components/Icon'
+import { TabBar } from 'antd-mobile'
+import { Outlet, useNavigate, useLocation } from 'react-router-dom'
+
+import styles from './index.module.scss'
+
+const tabs = [
+  { path: '/', icon: 'iconbtn_home', text: '首页' },
+  { path: '/question', icon: 'iconbtn_qa', text: '问答' },
+  { path: '/video', icon: 'iconbtn_video', text: '视频' },
+  { path: '/profile', icon: 'iconbtn_mine', text: '我的' }
+]
+
 const Layout = () => {
-  return <div>布局容器</div>
+  const navigate = useNavigate()
+  const location = useLocation()
+
+  const changeRoute = (path: string) => {
+    navigate(path)
+  }
+
+  return (
+    <div className={styles.root}>
+      <Outlet />
+      <TabBar className="tab-bar" activeKey={location.pathname} onChange={(key) => changeRoute(key)}>
+        {tabs.map((item) => (
+          <TabBar.Item
+            key={item.path}
+            icon={(active) => <Icon type={active ? `${item.icon}_sel` : item.icon} className="tab-bar-item-icon" />}
+            title={item.text}
+          />
+        ))}
+      </TabBar>
+    </div>
+  )
 }
 
 export default Layout
