ca8446fed5000fbd6da130190da0f28b4096437a
FlightLoong <1577886278@qq.com>
2022-02-03 18:49:37 +0800

    切换频道编辑状态

--- src/pages/Home/components/Channels/index.tsx
+++ src/pages/Home/components/Channels/index.tsx
@@ -1,11 +1,12 @@
-import classnames from 'classnames'
-
-import Icon from '@/components/Icon'
-import styles from './index.module.scss'
+import { useEffect, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+
+import classnames from 'classnames'
+import Icon from '@/components/Icon'
 import { RootState } from '@/types/store'
 import { getAllChannel } from '@/store/actions/home'
-import { useEffect } from 'react'
+
+import styles from './index.module.scss'
 
 type Props = {
   onClose: () => void
@@ -13,12 +14,19 @@ type Props = {
 
 const Channels = ({ onClose }: Props) => {
   const dispatch = useDispatch()
+  // 是否是编辑
+  const [isEdit, setIsEdit] = useState(false)
 
   useEffect(() => {
     dispatch(getAllChannel())
   }, [dispatch])
 
   const { userChannel, restChannel } = useSelector((state: RootState) => state.home)
+
+  // 切换编辑与保存状态
+  const onChangeEdit = () => {
+    setIsEdit(!isEdit)
+  }
 
   return (
     <div className={styles.root}>
@@ -27,11 +35,13 @@ const Channels = ({ onClose }: Props) => {
       </div>
       <div className="channel-content">
         {/* 编辑时，添加类名 edit */}
-        <div className={classnames('channel-item')}>
+        <div className={classnames('channel-item', isEdit && 'edit')}>
           <div className="channel-item-header">
             <span className="channel-item-title">我的频道</span>
             <span className="channel-item-title-extra">点击进入频道</span>
-            <span className="channel-item-edit">编辑</span>
+            <span className="channel-item-edit" onClick={onChangeEdit}>
+              {isEdit ? '保存' : '编辑'}
+            </span>
           </div>
           <div className="channel-list">
             {/* 选中时，添加类名 selected */}
