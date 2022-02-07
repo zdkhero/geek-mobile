77e9319ab2a09dd87b2fc478792763d73fe00d22
FlightLoong <1577886278@qq.com>
2022-02-07 18:14:58 +0800

    渲染文章内容

--- src/pages/Article/index.tsx
+++ src/pages/Article/index.tsx
@@ -3,6 +3,8 @@ import { useNavigate, useParams } from 'react-router-dom'
 import classNames from 'classnames'
 import styles from './index.module.scss'
 
+import DOMPurify from 'dompurify'
+
 import Icon from '@/components/Icon'
 import CommentItem from './components/CommentItem'
 import CommentFooter from './components/CommentFooter'
@@ -21,43 +23,66 @@ const Article = () => {
   }, [dispatch, params])
 
   const { detail } = useSelector((state: RootState) => state.article)
-  console.log(detail)
 
   const loadMoreComments = async () => {
     console.log('加载更多评论')
   }
 
   const renderArticle = () => {
+    // 解构出文章详情数据：
+    const {
+      // art_id,
+      // aut_id,
+      // // 是否点赞
+      // attitude,
+      // // 是否收藏
+      // is_collected,
+      content,
+      is_followed,
+      aut_name,
+      aut_photo,
+      comm_count,
+      like_count,
+      pubdate,
+      read_count,
+      title
+    } = detail
+
     // 文章详情
     return (
       <div className="wrapper">
         <div className="article-wrapper">
           <div className="header">
-            <h1 className="title">ES6 Promise 和 Async/await的使用</h1>
+            <h1 className="title">{title}</h1>
 
             <div className="info">
               <span>2019-03-11</span>
-              <span>202 阅读</span>
-              <span>10 评论</span>
+              <span>{read_count} 阅读</span>
+              <span>{comm_count} 评论</span>
             </div>
 
             <div className="author">
-              <img src="http://geek.itheima.net/images/user_head.jpg" alt="" />
-              <span className="name">黑马先锋</span>
-              <span className={classNames('follow', true ? 'followed' : '')}>{true ? '已关注' : '关注'}</span>
+              <img src={aut_photo || 'http://geek.itheima.net/images/user_head.jpg'} alt="" />
+              <span className="name">{aut_name}</span>
+              <span className={classNames('follow', is_followed ? 'followed' : '')}>{true ? '已关注' : '关注'}</span>
             </div>
           </div>
 
           <div className="content">
-            <div className="content-html dg-html" />
-            <div className="date">发布文章时间：2021-2-1</div>
+            <div
+              className="content-html dg-html"
+              dangerouslySetInnerHTML={{
+                __html: DOMPurify.sanitize(content)
+              }}
+            />
+            <div className="date">发布文章时间：{pubdate}</div>
           </div>
         </div>
 
         <div className="comment">
           <div className="comment-header">
             <span>全部评论（10）</span>
-            <span>20 点赞</span>
+            <span>{like_count} 点赞</span>
           </div>
 
           <div className="comment-list">
